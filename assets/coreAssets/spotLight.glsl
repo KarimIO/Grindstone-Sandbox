65546b75d2bec2e281fa2c9eae1d023f1d862638
KarimIO <karimah@aucegypt.edu>
2023-07-05 18:33:24 +0300

    Add Directional and Spot Light Shaders

---
+++ assets/coreAssets/spotLight.glsl
@@ -0,0 +1,219 @@
+#name Spot Light
+#renderQueue Lighting
+#shaderModule vertex
+#version 450
+
+layout(location = 0) in vec2 vertexPosition;
+
+layout(location = 0) out vec2 fragmentTexCoord;
+layout(location = 1) out vec3 fragmentViewRay;
+
+layout(binding = 0) uniform EngineUbo {
+	mat4 proj;
+	mat4 view;
+	vec3 eyePos;
+} ubo;
+
+void main() {
+	gl_Position = vec4(vertexPosition, 0.0, 1.0);
+	fragmentTexCoord = (vertexPosition * 0.5f) + vec2(0.5f);
+
+	vec3 positionVS = vec4(gl_Position * inverse(ubo.proj)).xyz;
+	fragmentViewRay = vec3(positionVS.xy / positionVS.z, 1.0f);
+}
+#endShaderModule
+#shaderModule fragment
+#version 450
+
+layout(location = 0) in vec2 fragmentTexCoord;
+layout(location = 1) in vec3 fragmentViewRay;
+layout(location = 0) out vec4 outColor;
+
+layout(binding = 0) uniform EngineUbo {
+	mat4 proj;
+	mat4 view;
+	vec3 eyePos;
+} ubo;
+
+layout(binding = 1) uniform sampler2D gbuffer0;
+layout(binding = 2) uniform sampler2D gbuffer1;
+layout(binding = 3) uniform sampler2D gbuffer2;
+layout(binding = 4) uniform sampler2D gbuffer3;
+layout(binding = 5) uniform sampler2D gbuffer4;
+
+layout(set = 1, binding = 0) uniform LightUbo {
+	mat4 shadowMatrix;
+	vec3 color;
+	float attenuationRadius;
+	vec3 position;
+	float intensity;
+	vec3 direction;
+	float innerAngle;
+	float outerAngle;
+	float shadowResolution;
+} light;
+
+const float pi = 3.14159f;
+
+// Schlick
+vec3 Light_F(in vec3 f0, in float f90, in float VH) {
+	return f0 + (f90-f0) * pow(1-VH, 5.0f);
+}
+
+// GGX
+float Light_D(in float alphaSqr, in float NH) {
+	float denom = NH * NH * (alphaSqr - 1) + 1;
+
+	return alphaSqr / (pi * denom * denom);
+}
+
+float Light_V( in float NL, in float NV, in float alphaSqr ) {
+	float Lambda_GGXV = NL * sqrt (( - NV * alphaSqr + NV ) * NV + alphaSqr );
+	float Lambda_GGXL = NV * sqrt (( - NL * alphaSqr + NL ) * NL + alphaSqr );
+
+	return 0.25f / ( Lambda_GGXV + Lambda_GGXL );
+}
+
+float Diff_Disney(float NdotV, float NdotL, float LdotH, float linearRoughness) {
+	float energyBias = 0 * (1-linearRoughness) + 0.5*linearRoughness;
+	float energyFactor = 1.0 * (1-linearRoughness) + linearRoughness / 1.51;
+	float fd90 = energyBias + 2.0 * LdotH * LdotH * linearRoughness;
+	vec3 f0 = vec3(1.0f , 1.0f , 1.0f);
+	float lightScatter = Light_F( f0 , fd90 , NdotL ) .r;
+	float viewScatter = Light_F(f0 , fd90 , NdotV ).r;
+
+	return lightScatter * viewScatter * energyFactor;
+}
+
+vec3 BSDF(float NV, float NL, float LH, float NH, float alpha, vec3 SpecularColor) {
+	vec3 f0 = 0.32*SpecularColor*SpecularColor;
+	float f90 = clamp(50 * dot(f0, vec3(0.33)), 0, 1);
+	
+	vec3 F = Light_F(f0, f90, LH);
+	float D = Light_D(alpha, NH);
+	float Vis = Light_V(NL, NV, alpha);
+
+	return (F * D * Vis);
+}
+
+vec3 LightPointCalc(
+	in vec3 albedo,
+	in vec3 position,
+	in vec3 specularTexture,
+	in float roughness,
+	in vec3 normal,
+	in vec3 lightPos,
+	in float lightRadius,
+	in vec3 lightColor,
+	in vec3 eyePos
+) {
+	vec3 lightDir	= position - lightPos;
+	float lightDistance	= length(lightDir);
+
+	vec3 eyeDir		= normalize(eyePos - position);
+	vec3 eyeReflect = reflect(-eyeDir, normal);
+	
+	lightDir		= -normalize(lightDir);
+
+	float alpha = roughness * roughness;
+	float alphaSqr = alpha * alpha;
+
+	float NL = clamp(dot(normal, lightDir), 0, 1);
+	
+	float distSqr = lightDistance * lightDistance;
+	float lightRadiusSqr = lightRadius * lightRadius;
+	float attenuationFactor = distSqr / lightRadiusSqr;
+	float attenuation = clamp(1 - attenuationFactor * attenuationFactor, 0, 1); 
+	attenuation = attenuation * attenuation / (distSqr + 0.001);
+
+	vec3 H = normalize(eyeDir + lightDir);
+	
+	float NV = abs(dot(normal, eyeDir)) + 0.00001;
+	float NH = clamp(dot(normal, H), 0, 1);
+	float LH = clamp(dot(lightDir, H), 0, 1);
+	float VH = clamp(dot(eyeDir, H), 0, 1);
+
+	vec3 specular = BSDF(NV, NL, LH, NH, alpha, specularTexture.rgb);
+	vec3 diffuse = albedo.rgb * vec3(Diff_Disney(NV, NL, LH, roughness)) / pi;
+
+	vec3 lightModifier = lightColor.xyz * attenuation;
+	vec3 BSDFValue = diffuse + specular;
+	return vec3(NL * BSDFValue * lightModifier);
+}
+
+vec2 poissonDisk[16] = vec2[]( 
+   vec2( -0.94201624, -0.39906216 ), 
+   vec2( 0.94558609, -0.76890725 ), 
+   vec2( -0.094184101, -0.92938870 ), 
+   vec2( 0.34495938, 0.29387760 ), 
+   vec2( -0.91588581, 0.45771432 ), 
+   vec2( -0.81544232, -0.87912464 ), 
+   vec2( -0.38277543, 0.27676845 ), 
+   vec2( 0.97484398, 0.75648379 ), 
+   vec2( 0.44323325, -0.97511554 ), 
+   vec2( 0.53742981, -0.47373420 ), 
+   vec2( -0.26496911, -0.41893023 ), 
+   vec2( 0.79197514, 0.19090188 ), 
+   vec2( -0.24188840, 0.99706507 ), 
+   vec2( -0.81409955, 0.91437590 ), 
+   vec2( 0.19984126, 0.78641367 ), 
+   vec2( 0.14383161, -0.14100790 ) 
+);
+
+float ShadowRandom(vec4 seed4) {
+	float dotProduct = dot(seed4, vec4(12.9898,78.233,45.164,94.673));
+    return fract(sin(dotProduct) * 43758.5453);
+}
+
+float GetShadowValue(in vec3 pos, in float nl) {
+	vec4 shadowCoord = light.shadowMatrix * vec4(pos,1);
+	vec3 shadowCoordsFinal = shadowCoord.xyz / shadowCoord.w;
+	float bias = 0.005;
+
+	float visibility = 1.0f;
+	for (int i=0; i < 4; i++) {
+        // int index = int(16.0 * ShadowRandom(vec4(floor(pos.xyz * 1000.0), i))) % 16;
+        // visibility -= 0.25 * (1.0 - texture(shadowMap, vec3(shadowCoordsFinal.xy + poissonDisk[index] / light.shadowResolution, shadowCoordsFinal.z - bias)));
+    }
+
+	return visibility;
+}
+
+void main() {
+	vec4 gbuffer3Value = texture(gbuffer3, fragmentTexCoord);
+
+	vec3 position = texture(gbuffer0, fragmentTexCoord).rgb;
+	vec3 diffuse = texture(gbuffer1, fragmentTexCoord).rgb;
+	vec3 normal = texture(gbuffer2, fragmentTexCoord).rgb;
+	vec3 specular = gbuffer3Value.rgb;
+	float roughness = gbuffer3Value.a;
+
+	vec3 lightPow = light.color * light.intensity;
+	
+	vec3 lightDir = normalize(position - light.position);
+	vec3 lightDirection = light.direction;
+
+	float maxDot = cos(light.innerAngle);
+	float minDot = cos(light.outerAngle);
+	float diffDot = maxDot - minDot;
+	float dotPR = dot(lightDir, lightDirection);
+	dotPR = clamp((dotPR - minDot) / diffDot, 0, 1);
+
+	vec3 litValues = max(
+		LightPointCalc(
+			diffuse,
+			position,
+			specular,
+			roughness,
+			normal,
+			light.position,
+			light.attenuationRadius,
+			lightPow,
+			ubo.eyePos
+		) * dotPR,
+		0.0f
+	);
+
+	outColor = vec4(litValues, 1);
+}
+#endShaderModule
