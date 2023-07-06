0d53f1179ba1cda7fa950de882151c77c70c2937
KarimIO <karimah@aucegypt.edu>
2023-07-06 19:34:47 +0300

    Shadow Maps

---
+++ assets/coreAssets/shadowMap.glsl
@@ -0,0 +1,25 @@
+#name Shadow Mapping
+#renderQueue Lighting
+#shaderModule vertex
+#version 450
+
+layout(location = 0) in vec3 vertexPosition;
+
+layout(binding = 0) uniform LightUbo {
+	mat4 shadowMatrix;
+	vec3 color;
+	float sourceRadius;
+	vec3 direction;
+	float intensity;
+	float shadowResolution;
+} light;
+
+void main() {
+	gl_Position = light.shadowMatrix * vec4(vertexPosition, 1.0);
+}
+#endShaderModule
+#shaderModule fragment
+#version 450
+
+void main() {}
+#endShaderModule
